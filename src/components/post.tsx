7206cc53d7614fb8d752f8450691f64515b15f40
gron1gh1 <gron1gh1@gmail.com>
2020-11-03 23:58:39 +0900

    Add: Transition Header - 1

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -4,7 +4,7 @@ import { ITemplateProps } from "../interface"
 import SEO from "./seo"
 import Comment from "./comment"
 import "./post.css"
-import PageTransition from 'gatsby-plugin-page-transitions';
+import PageTransition from "gatsby-plugin-page-transitions"
 
 type IPostTemplateProps = ITemplateProps<{
   html: string
@@ -13,14 +13,29 @@ type IPostTemplateProps = ITemplateProps<{
 
 const Post: React.FC<IPostTemplateProps> = React.memo(props => {
   return (
-    <PageTransition>
-      <Layout headerHeight="100px">
+    <PageTransition
+      defaultStyle={{
+        transition: 'left 500ms cubic-bezier(0.47, 0, 0.75, 0.72)',
+        height: "30vh",
+      }}
+      transitionStyles={{
+        entering: { height: "100px" },
+        entered: { height: "100px" },
+        exiting: { height: "30vh" },
+      }}
+      transitionTime={500}
+    >
+      <Layout >
         <SEO
           title={props.pageContext.title}
           description={props.pageContext.html}
         />
-        <div dangerouslySetInnerHTML={{ __html: props.pageContext.html }}></div>
-        <Comment repo="gron1gh1/blog" title={props.pageContext.title} />
+        <PageTransition>
+          <div
+            dangerouslySetInnerHTML={{ __html: props.pageContext.html }}
+          ></div>
+          <Comment repo="gron1gh1/blog" title={props.pageContext.title} />
+        </PageTransition>
       </Layout>
     </PageTransition>
   )
