aeb321e53adc7f3dc1fc82dd579dad3e0abc6854
gron1gh1 <gron1gh1@gmail.com>
2020-11-03 13:05:11 +0900

    Add: Markdown View

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import Layout from './layout';
-    
-const PostTemplate: React.FC = React.memo(props => {
-    return (
-        <Layout>
-            <code>
-                <pre>{JSON.stringify(props, null, 4)}</pre>
-            </code>
-        </Layout>
-    );
-});
+import React from "react"
+import Layout from "./layout"
+import { ITemplateProps } from "../interface"
 
-export default PostTemplate;
+type IPostTemplateProps = ITemplateProps<{
+  html: string
+  title: string
+}>
+
+const Post: React.FC<IPostTemplateProps> = React.memo(props => {
+  return (
+    <Layout>
+      <div dangerouslySetInnerHTML={{ __html: props.pageContext.html }}></div>
+    </Layout>
+  )
+})
+
+export default Post
