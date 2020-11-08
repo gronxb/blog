4ca50ff3a0719997f8780bbcb707d00bfc603e0b
gron1gh1 <gron1gh1@gmail.com>
2020-11-08 19:45:12 +0900

    Add: logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -34,27 +34,23 @@ function Header({
       defaultStyle={defaultStyle}
       transitionStyles={transitionStyle}
     >
-      <header>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `1.45rem 1.0875rem`,
-          }}
-        >
-          <h1 style={{ margin: 0 }}>
-            <Link
-              to="/"
-              style={{
-                color: `white`,
-                textDecoration: `none`,
-              }}
-            >
-              {siteTitle}
-            </Link>
-          </h1>
-        </div>
-      </header>
+      <Link
+        to="/"
+        style={{
+          color: `white`,
+          textDecoration: `none`,
+        }}
+      >
+        <header>
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `1.45rem 1.0875rem`,
+            }}
+          ></div>
+        </header>
+      </Link>
     </PageTransition>
   )
 }
