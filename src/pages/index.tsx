492f84069a6f49350e9efd9f6121a7cbce934511
gron1gh1 <gron1gh1@gmail.com>
2020-11-19 22:47:54 +0900

    Modify: index.tsx styled-components => scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,94 +34,13 @@ const LatestPostListQuery = graphql`
   }
 `
 
-const ProfileBox = styled.div`
-  flex: 1;
-  border-radius: 10px;
-  margin-top: 30px;
-  padding-bottom: 2rem;
-  height: 200px;
-  display: flex;
-  align-items: center;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-    text-align: center;
-    height: 100%;
-  }
-`
-
-const ProfileImg = styled.div`
-  width: 200px;
-  height: 200px;
-  border-radius: 70%;
-  overflow: hidden;
-  flex: none;
-  margin-left: 10px;
-
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  @media (max-width: 768px) {
-    width: 150px;
-    height: 150px;
-  }
-`
-
-const ProfileContent = styled.div`
-  margin-left: 30px;
-  .name {
-    font-size: 1.5rem;
-    font-weight: bold;
-    margin-bottom: 15px;
-    display: flex;
-
-    div {
-      margin-top: -2px;
-      margin-left: 5px;
-    }
-  }
-  .link{
-    margin-top: 0.7rem;
-  }
-  .icon {
-    display: flex;
-    align-items: center;
-    font-size: 0.8rem;
-    font-weight: bold;
-    a{
-      text-decoration: none;
-      &:link, &:visited{
-        color:black;
-      }
-    }  
-  }
-  
-  @media (max-width: 768px) {
-    margin-top: 20px;
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    .icon {
-      justify-content: center;
-    }
-  }
-  @media (max-width: 440px) {
-    .content {
-      font-size: 80%;
-    }
-  }
-`
-
 const Profile = () => {
   return (
-    <ProfileBox>
-      <ProfileImg>
+    <div className="profile-box">
+      <div className="profile-img">
         <img src="https://avatars2.githubusercontent.com/u/41789633?s=460&u=78fa3dc9f8cabfc341f994074aac031e249e1ae3&v=4" />
-      </ProfileImg>
-      <ProfileContent>
+      </div>
+      <div className="profile-content">
         <p className="name">강선규</p>
         <div className="content">
           <p>이것저것 다하는 풀스택 지향 개발자입니다😉</p>
@@ -144,8 +63,8 @@ const Profile = () => {
             </div>
           </div>
         </div>
-      </ProfileContent>
-    </ProfileBox>
+      </div>
+    </div>
   )
 }
 const IndexView = () => {
